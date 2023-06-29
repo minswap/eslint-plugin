a4d2fcb39edf16fba60cc69c30323f810672a389
Parichay <parichay.barpanda@gmail.com>
2023-06-29 14:57:19 +0530

    Cover all test cases and implemnet checks

--- src/result-type-handling.ts
+++ src/result-type-handling.ts
@@ -1,11 +1,18 @@
-import { ESLintUtils, TSESTree } from "@typescript-eslint/utils";
+import { AST_NODE_TYPES, ESLintUtils } from "@typescript-eslint/utils";
+
+import type { TSESTree } from "@typescript-eslint/utils";
+import { RuleContext } from "@typescript-eslint/utils/dist/ts-eslint";
 
 export const createRule = ESLintUtils.RuleCreator(
   (name) => `https://typescript-eslint.io/rules/${name}`
 );
 
+const RESULT_PROPERTIES = ["ok", "err"];
+
+const RESULT_TYPES = ["Ok", "Err"];
+
 export default createRule({
-  name: "result-type-handling",
+  name: "result-handling",
   meta: {
     type: "suggestion",
     docs: {
@@ -20,31 +27,274 @@ export default createRule({
   defaultOptions: [],
   create(context) {
     return {
-      CallExpression(node): void {
-        const returnType = getTypeOfReturnValue(node);
-        if (returnType === "number") {
-          context.report({
-            node,
-            messageId: "resultHandling",
-          });
+      CallExpression(node) {
+        /**
+         *
+         * If the call expression returns a Ok or Err type - NO ERROR!
+         *
+         * see first test in test/result-handling.test.ts
+         */
+        const okOrErr = isOkOrErr(node);
+        if (okOrErr) {
+          return;
+        }
+        /**
+         * When the call expression is unwrap - NO ERROR!
+         *
+         * See test2(), test3() in test/result-handling.test.ts
+         *
+         */
+        const unWrap = isUnwrapCallExpr(node);
+        if (unWrap) {
+          return;
+        }
+        const resultType = isResultType(context, node);
+        // if the call expression returns a Result type
+        if (resultType) {
+          /**
+           * If the parent of the call expression is unwrap - NO ERROR!
+           *
+           * See test 2(), test3() in test/result-handling.test.ts
+           */
+          if (isParentUnwrapCallExpr(node)) {
+            return;
+          }
+          /**
+           * If the parent of the call expression is not unwrap, then we need to check:
+           *
+           * 1. If it is assigned to a variable
+           * 2. If it is returned directly
+           * 3. If it is not assigned to a variable
+           */
+          const variableName = isVariableDeclaration(getParent(node));
+          /**
+           * Case 1: If the result is assigned to a variable.
+           */
+          if (variableName) {
+            // get the function body of the variable declaration
+            const functionBody = findParentFunctionBody(node);
+            if (functionBody) {
+              const index = functionBody?.findIndex(
+                (statement) => statement === node.parent?.parent
+              );
+              // get statements after the variable declaration
+              const restStatements = functionBody.slice(index + 1);
+              /**
+               *
+               * @variableName = the variable assigned with Result returned from the call expression
+               *
+               * We do following checks for statements in function body:
+               *
+               * 1.There is a unwrap call expression with variableName
+               * 2. There is a return statement with variableName
+               * 3. There is a if/ternary condition to check for variableName
+               *
+               * If non above conidtion is true - REPORT ERROR!
+               */
+              // see test4(), test5() in test/result-handling.test.ts
+              const unwrap = restStatements.some((statement) =>
+                doesUnwrap(statement, variableName)
+              );
+
+              // TODO: check for ternary operator
+              const doesConditionCheck = restStatements.some(
+                (statement) =>
+                  statement.type === "IfStatement" &&
+                  statement.test.type === "BinaryExpression" &&
+                  statement.test.left.type === "Identifier" &&
+                  statement.test.left.name === variableName &&
+                  statement.test.right.type === "Literal" &&
+                  statement.test.right.value === "condition"
+              );
+
+              // see test6() in test/result-handling.test.ts
+              const doesReturn = restStatements.some(
+                (statement) =>
+                  statement.type === "ReturnStatement" &&
+                  statement.argument?.type === "Identifier" &&
+                  statement.argument.name === variableName
+              );
+
+              if (!unwrap && !doesReturn && !doesConditionCheck) {
+                context.report({
+                  node,
+                  messageId: "resultHandling",
+                });
+              }
+            }
+          }
+          /**
+           * Case 2: If the result is returned directly - NO ERROR!
+           * See test8() in test/result-handling.test.ts
+           */
+          const returnStatement = isReturnStatement(getParent(node));
+          /**
+           * Case 3: If result is not returned directly and not assigned to a variable - REPORT ERROR!
+           *
+           * See test9() in test/result-handling.test.ts
+           */
+          if (!returnStatement && !variableName) {
+            context.report({
+              node,
+              messageId: "resultHandling",
+            });
+          }
         }
       },
     };
   },
 });
 
-function getTypeOfReturnValue(node: TSESTree.CallExpression): string {
-  // Check if the function call has a return statement
-  if (node.parent && node.parent.type === "ReturnStatement") {
-    const returnExpression = node.parent.argument;
-    if (
-      returnExpression?.type === "Literal" &&
-      typeof returnExpression.value === "number"
-    ) {
-      return "number";
+/** Utilities */
+
+function getParent(node: TSESTree.Node): TSESTree.Node | undefined {
+  return node.parent;
+}
+
+function isReturnStatement(node: TSESTree.Node | undefined): boolean {
+  if (node && node.type === AST_NODE_TYPES.ReturnStatement) {
+    return true;
+  }
+  return false;
+}
+
+function isVariableDeclaration(
+  node: TSESTree.Node | undefined
+): string | false {
+  if (node && node.type === AST_NODE_TYPES.VariableDeclarator) {
+    const variableName = getVariableName(node);
+    if (variableName) return variableName;
+  }
+  return false;
+}
+
+function getVariableName(node: TSESTree.Node | undefined): string | undefined {
+  if (node && node.type === AST_NODE_TYPES.VariableDeclarator) {
+    if (node.id.type === AST_NODE_TYPES.Identifier) {
+      return node.id.name;
     }
   }
+  return undefined;
+}
 
-  // Default to 'unknown' if the return type cannot be determined
-  return "unknown";
+const isOkOrErr = (node: TSESTree.CallExpression): boolean => {
+  if (node.callee.type === AST_NODE_TYPES.MemberExpression) {
+    if (node.callee.object.type === AST_NODE_TYPES.Identifier) {
+      if (node.callee.object.name === "Result") {
+        if (node.callee.property.type === AST_NODE_TYPES.Identifier) {
+          const propertyName = node.callee.property.name;
+          if (
+            RESULT_PROPERTIES.some((resultType) => propertyName === resultType)
+          ) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  return false;
+};
+
+function isParentUnwrapCallExpr(node: TSESTree.CallExpression): boolean {
+  const parent = node.parent;
+  if (parent && parent.type === AST_NODE_TYPES.CallExpression) {
+    if (isUnwrapCallExpr(parent)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const isUnwrapCallExpr = (node: TSESTree.CallExpression): boolean => {
+  if (node.callee.type === AST_NODE_TYPES.MemberExpression) {
+    if (node.callee.object.type === AST_NODE_TYPES.Identifier) {
+      if (node.callee.object.name === "Result") {
+        if (node.callee.property.type === AST_NODE_TYPES.Identifier) {
+          const propertyName = node.callee.property.name;
+          if (propertyName === "unwrap") {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  return false;
+};
+
+function doesUnwrap(node: TSESTree.Statement, variableName: string): boolean {
+  let unwrapStatement = isUnwrapStatment(node, variableName);
+  if (node.type === AST_NODE_TYPES.VariableDeclaration) {
+    const declaration = node.declarations[0].init;
+    if (declaration && declaration.type === AST_NODE_TYPES.CallExpression) {
+      unwrapStatement = isUnwrapCallExpr(declaration);
+    }
+  }
+  return unwrapStatement;
+}
+
+function isUnwrapStatment(
+  statement: TSESTree.Statement,
+  variableName: string
+): boolean {
+  return (
+    statement.type === AST_NODE_TYPES.ExpressionStatement &&
+    statement.expression.type === AST_NODE_TYPES.CallExpression &&
+    statement.expression.callee.type === AST_NODE_TYPES.MemberExpression &&
+    statement.expression.callee.object.type === AST_NODE_TYPES.Identifier &&
+    statement.expression.callee.object.name === "Result" &&
+    statement.expression.callee.property.type === AST_NODE_TYPES.Identifier &&
+    statement.expression.callee.property.name === "unwrap" &&
+    statement.expression.arguments.length === 1 &&
+    statement.expression.arguments[0].type === AST_NODE_TYPES.Identifier &&
+    statement.expression.arguments[0].name === variableName
+  );
+}
+
+function isResultType(
+  context: Readonly<RuleContext<"resultHandling", never[]>>,
+  node: TSESTree.CallExpression
+): boolean {
+  const returnType = getCallExpressionReturnType(context, node);
+  if (
+    returnType &&
+    RESULT_TYPES.some((resultType) => returnType.includes(resultType))
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function getCallExpressionReturnType(
+  context: Readonly<RuleContext<"resultHandling", never[]>>,
+  node: TSESTree.CallExpression
+): string | null {
+  const parserServices = context.parserServices;
+  if (!parserServices || !parserServices.program) {
+    return null;
+  }
+
+  const typeChecker = parserServices.program.getTypeChecker();
+  // Get the TypeScript type of the CallExpression
+  const type = typeChecker.getTypeAtLocation(
+    parserServices.esTreeNodeToTSNodeMap.get(node.callee)
+  );
+
+  // Get the return type of the CallExpression
+  const returnType = type.getCallSignatures()[0].getReturnType();
+
+  return typeChecker.typeToString(returnType);
+}
+
+function findParentFunctionBody(
+  node: TSESTree.Node
+): TSESTree.Statement[] | undefined {
+  let currentNode: TSESTree.Node | undefined = node.parent;
+  while (currentNode) {
+    // TODO: Add support for arrow functions
+    if (currentNode.type === AST_NODE_TYPES.BlockStatement) {
+      return currentNode.body;
+    }
+    currentNode = currentNode.parent;
+  }
+  return undefined;
 }
--- test/result-handling.test.ts
+++ test/result-handling.test.ts
@@ -1,24 +1,115 @@
-import * as parser from "@typescript-eslint/parser";
 import { ESLintUtils } from "@typescript-eslint/utils";
 
 const ruleTester = new ESLintUtils.RuleTester({
   parser: "@typescript-eslint/parser",
 });
 
-import rule from "../src/result-type-handling";
+import resultTypeHandlingRule from "../src/result-type-handling";
 
-ruleTester.run("result handling", rule, {
-  valid: [],
+const resultTypeDeclaration = `
+  export type Ok<T> = { type: "ok"; value: T };
+  export type Err<E> = { type: "err"; error: E };
+
+  export type Result<T, E> = Ok<T> | Err<E>;
+
+  export namespace Result {
+    export const ok = <T>(value: T): Ok<T> => ({ type: "ok", value });
+
+    export const err = <E>(error: E): Err<E> => ({ type: "err", error });
+
+    export const unwrap = <T, E>(result: Result<T, E>): T => {
+      if (result.type === "ok") {
+        return result.value;
+      }
+      throw result.error;
+    };
+  }
+
+  function getResult(foo?: number) { 
+    if(foo && foo > 1) {
+      return Result.err("foo must be less than 1")
+    }  
+    return Result.ok(1) 
+  }
+`;
+
+const wrapResultDeclaration = (code: string) => `
+  ${resultTypeDeclaration}
+  ${code}
+`;
+
+ruleTester.run("result handling", resultTypeHandlingRule, {
+  valid: [
+    wrapResultDeclaration(``),
+    wrapResultDeclaration(`
+      function test2() {
+        const res = Result.unwrap(getResult(0))
+        return res
+      }
+    `),
+    wrapResultDeclaration(`
+      function test3() {
+        return Result.unwrap(getResult(0))
+      }
+    `),
+    wrapResultDeclaration(`
+      function test4() {
+        const res = getResult(0)
+        Result.unwrap(res)
+        return 2
+      }
+    `),
+    wrapResultDeclaration(`
+      function test5() {
+        const res = getResult(0)
+        const foo = 1
+        const bar = Result.unwrap(res)
+        return bar
+      }
+    `),
+    wrapResultDeclaration(`
+      function test6() {
+        const res = getResult()
+        return res
+      }
+    `),
+    wrapResultDeclaration(`
+      function test7() {
+        const res = getResult()
+        if (res.type === "ok") {
+          return res.value
+        }
+        throw new Error(res.value)
+      }
+    `),
+    wrapResultDeclaration(`
+      function test8() {
+        return getResult()
+      }
+    `),
+    wrapResultDeclaration(`
+      function test1() { return 1 }
+      test1()
+    `),
+    wrapResultDeclaration(`
+    function getFakeResult() {
+      return 1
+    }
+    function test() {
+      const res = getFakeResult()
+      return res
+    }
+  `),
+  ],
   invalid: [
     {
-      code: `
-      const test = (): void => {
-        const a = test2();
-        console.info(a);
-      };
-      
-      const test2 = (): number => 2;
-      `,
+      code: wrapResultDeclaration(`
+      function test9() {
+        const foo = 1
+        getResult()
+        return foo
+      }
+    `),
       errors: [{ messageId: "resultHandling" }],
     },
   ],
